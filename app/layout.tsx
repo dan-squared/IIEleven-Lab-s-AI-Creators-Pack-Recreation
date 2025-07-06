2a3ffd8aa60d0b74723e4b76f354a348bf568fd8
Daniel G. <daniel.creativesparks@gmail.com>
2025-07-06 10:40:19 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'AI Creators Pack',
   description: 'Recreation of Eleven Lab AI creator Pack',
- ,
 }
 
 export default function RootLayout({
